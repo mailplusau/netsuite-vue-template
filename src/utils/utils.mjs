26f9098908f5849ec555cd132e8cc3f09fd0fe57
Tim Nguyen <tim.nguyen@mailplus.com.au>
2023-09-11 15:53:30 +1000

    Add validation rules

--- src/utils/utils.mjs
+++ src/utils/utils.mjs
@@ -2,6 +2,68 @@
 export const VARS = {
     pageTitle: 'Template Page'
 }
+
+export const baseURL = 'https://' + process.env.VUE_APP_NS_REALM + '.app.netsuite.com';
+
+export const rules = {
+    email(value, fieldName = 'This field') {
+        return !value || /.+@.+\..+/.test(value) || `${fieldName} must be a valid email`;
+    },
+    required(value, fieldName = 'This field') {
+        return !!value || `${fieldName} is required`;
+    },
+    minLength(value, fieldName = 'This field', length) {
+        return (value && value.length >= length) || `${fieldName} must be more than ${length} characters`;
+    },
+    abn(value, fieldName = 'This field') {
+        if (!value) return true;
+
+        let weights = [10, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19],
+            checksum = value.split('').map(Number).reduce(
+                function(total, digit, index) {
+                    if (!index) {
+                        digit--;
+                    }
+                    return total + (digit * weights[index]);
+                },
+                0
+            );
+
+        return value.length === 11 || !(!checksum || checksum % 89 !== 0) || `${fieldName} must be a valid ABN`;
+    },
+    ausPhone(value, fieldName = 'This field') {
+        let australiaPhoneFormat = /^(\+\d{2}[ -]{0,1}){0,1}(((\({0,1}[ -]{0,1})0{0,1}\){0,1}[2|3|7|8]{1}\){0,1}[ -]*(\d{4}[ -]{0,1}\d{4}))|(1[ -]{0,1}(300|800|900|902)[ -]{0,1}((\d{6})|(\d{3}[ -]{0,1}\d{3})))|(13[ -]{0,1}([\d -]{5})|((\({0,1}[ -]{0,1})0{0,1}\){0,1}4{1}[\d -]{8,10})))$/;
+        return !value || australiaPhoneFormat.test(value) || `${fieldName} must be a valid phone number`;
+    },
+
+    validate(v, validationString, fieldName = 'This field') {
+        let validations = validationString.split('|');
+
+        for (let validation of validations) {
+            if (validation === 'validate') continue;
+
+            let terms = validation.split(':');
+            let rule = terms.shift();
+            terms = terms.length ? terms[0].split(',') : [];
+            let result = rules[rule] ? rules[rule](v, fieldName || 'Field', ...terms) : null;
+
+            if (typeof result === 'string') return result;
+        }
+
+        return true
+    }
+}
+
+export function allowOnlyNumericalInput(evt) {
+    // evt = (evt) ? evt : window.event;
+    let expect = evt.target.value.toString() + evt.key.toString();
+
+    // if (!/^[-+]?[0-9]*?[0-9]*$/.test(expect)) // Allow only 1 leading + sign and numbers
+    if (!/^[0-9]*$/.test(expect)) // Allow only numbers
+        evt.preventDefault();
+    else return true;
+}
+
 export function debounce(fn, wait){
     let timer;
     return function(...args){
