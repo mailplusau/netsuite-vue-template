c14a0c90c43e4ec31cc8a5e1efff9b254d6ebc3e
Tim Nguyen <tim.nguyen@mailplus.com.au>
2023-09-13 00:43:04 +1000

    Update page's title

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import {VARS} from '@/utils/utils.mjs'
 
 Vue.config.productionTip = false
 
-if (parent['setMPTheme']) parent.setMPTheme('Test Page - NetSuite Australia (Mail Plus Pty Ltd)')
+if (parent['setMPTheme']) parent.setMPTheme(`${VARS.pageTitle} - NetSuite Australia (Mail Plus Pty Ltd)`)
 
 new Vue({
   store,
