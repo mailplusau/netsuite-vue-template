11e598c90ecb11b55795858641de7cb27481c84a
Tim Nguyen <tim.nguyen@mailplus.com.au>
2023-09-13 11:20:44 +1000

    Add an example vuex module

---
+++ src/store/modules/example.js
@@ -0,0 +1,24 @@
+const state = {
+
+};
+
+const getters = {
+
+};
+
+const mutations = {
+
+};
+
+const actions = {
+    init : () => {
+        console.log('init');
+    },
+};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+};
